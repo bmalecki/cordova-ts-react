466ea7c560c7aeb7fa95240b0e7f80c1322df9b2
bmalecki <bmalecki995@gmail.com>
2016-08-31 12:33:32 +0200

    edit install script

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #/bin/bash
 
 PACKAGE=$(sed -n '/<widget /s/.*id="\([^"]*\)"[^\n]*/\1/p' config.xml)
+ANDROID_PROCESSOR_ARCH=${ANDROID_PROCESSOR_ARCH:-armv7}
 
-npm run build
 adb connect 192.168.1.13
+npm run build
 cordova build android
-adb install -r $(pwd)/platforms/android/build/outputs/apk/android-armv7-debug.apk
+adb install -r \
+    $(pwd)/platforms/android/build/outputs/apk/android-${ANDROID_PROCESSOR_ARCH}-debug.apk
 adb shell am start -n ${PACKAGE}/${PACKAGE}.MainActivity
